51610468b5beb80bbfcfe34756c20d29569193ae
jkarrer <jkarrer@protect247.com>
2023-03-23 08:55:42 -0400

    :sparkles: stream connection impl function

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
-use std::io::{Read,Write};
+use std::io::{Read, Write, Error, ErrorKind};
 use std::net::{TcpStream, SocketAddr, ToSocketAddrs};
+use std::time::Duration;
+
 
 struct Request {
     method: String,
     resource_path: String,
-    host: String
+    host: String,
+    port: i16
 }
 
 impl Request {
@@ -12,19 +15,28 @@ impl Request {
         let r = format!("{} {} HTTP/1.1\r\nHost: {}\r\n\r\n", &self.method, &self.resource_path, &self.host);
         return r;
     }
-    fn make_stream_connection(&self) -> Result<TcpStream, std::io::Error> {
-        let host = "127.0.0.1";
-        let port = 8080;
-        let address = format!("{}:{}", host, port);
-        return TcpStream::connect(address);
+    fn make_stream_connection(&self) -> Result<TcpStream, Error> {
+        let address = format!("{}:{}", &self.host, &self.port);
+        let possible_ip_addresses = address.to_socket_addrs().unwrap();
+        
+        for addr in possible_ip_addresses {
+            let ok_connection = match TcpStream::connect_timeout(&addr, Duration::new(2, 0)) {
+                Ok(stream) => return Ok(stream),
+                Err(_e) => {
+                    continue
+                }
+            };
+        }
+        Err(Error::new(ErrorKind::Other, "Failed to connect to any address"))
     }
 }
 
 fn main() {
     let req = Request {
         method: String::from("GET"),
-        resource_path: String::from("/api"),
-        host: String::from("localhost")
+        resource_path: String::from("/"),
+        host: String::from("www.google.com"),
+        port: 80
     };
    
     let mut stream = req.make_stream_connection().unwrap();
